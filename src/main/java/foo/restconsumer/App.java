ba31bfebb7cbd1e0bf56281e6058eb06798a7890
Victor Moreira <victorpmoreira@gmail.com>
2015-05-17 23:34:19 -0300

    Rest consumer imp.

--- src/main/java/foo/restconsumer/App.java
+++ src/main/java/foo/restconsumer/App.java
@@ -1,13 +1,17 @@
 package foo.restconsumer;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.web.client.RestTemplate;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String args[]) {
+        RestTemplate restTemplate = new RestTemplate();
+        Photo photo = restTemplate.getForObject("http://jsonplaceholder.typicode.com/photos/5", Photo.class);
+        System.out.println("Album Id:       " + photo.getAlbumId());
+        System.out.println("Id:             " + photo.getId());
+        System.out.println("Title:          " + photo.getTitle());
+        System.out.println("Url:            " + photo.getUrl());
+        System.out.println("Thumbnail url:  " + photo.getThumbnailUrl());
     }
+
 }
